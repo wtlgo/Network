3c85dda304426d6bacca4003d5daf2c32d7cb85b
wtlgo <50925676+wtlgo@users.noreply.github.com>
2019-12-30 04:08:04 +0300

    Hope to make things faster

--- src/Network.cpp
+++ src/Network.cpp
@@ -58,6 +58,8 @@ std::string Network::request(std::string url, const std::map<std::string, std::s
             }
         }
         
+        curl_easy_setopt(curl.get(), CURLOPT_SSL_VERIFYPEER, 0L);
+        curl_easy_setopt(curl.get(), CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
         curl_easy_setopt(curl.get(), CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
         curl_easy_setopt(curl.get(), CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, &readBuffer);
