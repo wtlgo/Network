50e83a0fbffd9e91d19af280d7e56f71998e138c
wtlgo <georgeendo0@gmail.com>
2023-01-23 17:37:28 +0300

    Add DefaultConfig.Clone test

--- test/src/DefaultConfig.cpp
+++ test/src/DefaultConfig.cpp
@@ -9,3 +9,14 @@ TEST(DefaultConfig, Create) {
     const Config::cptr_t config = DefaultConfig::create();
     ASSERT_NE(config, nullptr);
 }
+
+TEST(DefaultConfig, Clone) {
+    using namespace wtlgo::network;
+
+    const Config::cptr_t config = DefaultConfig::create();
+    ASSERT_NE(config, nullptr);
+
+    const Config::cptr_t clone = config->clone();
+    ASSERT_NE(clone, nullptr);
+    ASSERT_NE(clone, config);
+}
