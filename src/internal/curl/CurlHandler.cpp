10cefc0311dcb154ba33dae2f656f4bed0dbe37c
wtlgo <georgeendo0@gmail.com>
2023-02-02 15:34:36 +0300

    Create CurlHandler template

---
+++ src/internal/curl/CurlHandler.cpp
@@ -0,0 +1,1 @@
+#include "./CurlHandler.hpp"
---
+++ src/internal/curl/CurlHandler.hpp
@@ -0,0 +1,14 @@
+#ifndef __WTLGO__NETWORK__INTERNAL__CURL__CURL_HANDLER__
+#define __WTLGO__NETWORK__INTERNAL__CURL__CURL_HANDLER__
+
+namespace wtlgo {
+namespace network {
+namespace internal {
+namespace curl {
+class CurlHandler {};
+}
+}
+}
+}
+
+#endif
