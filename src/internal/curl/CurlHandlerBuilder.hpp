98039d05cde9cad4259793587c4a9bc5e5acb378
wtlgo <georgeendo0@gmail.com>
2023-02-02 16:04:36 +0300

    Add CurlHandler Builder template

---
+++ src/internal/curl/CurlHandlerBuilder.hpp
@@ -0,0 +1,14 @@
+#ifndef __WTLGO__NETWORK__INTERNAL__CURL__CURL_HANDLER_BUILDER__
+#define __WTLGO__NETWORK__INTERNAL__CURL__CURL_HANDLER_BUILDER__
+
+namespace wtlgo {
+namespace network {
+namespace internal {
+namespace curl {
+class CurlHandlerBuilder {};
+}
+}
+}
+}
+
+#endif
